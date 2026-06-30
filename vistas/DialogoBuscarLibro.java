d60fa6028ae26937ee2c11f9fb8cf2abe4e0a956
Xiomara Nuñez <xiomaranunezccupa@gmail.com>
2026-06-30 01:03:26 -0500

    panel superior , ingreso de datos

--- vistas/DialogoBuscarLibro.java
+++ vistas/DialogoBuscarLibro.java
@@ -14,5 +14,11 @@ public DialogoBuscarLibro(JFrame ventanaPadre, GestorBiblioteca gestor) {
         setSize(400, 300);
         setLocationRelativeTo(ventanaPadre);
         setLayout(new BorderLayout(10, 10));
-    
+    JPanel panelBusqueda = new JPanel(new FlowLayout());
+    panelBusqueda.add(new JLabel("Ingrese el Código del Libro:"));
+    txtCodigoBuscar = new JTextField(10); // Caja de texto de 10 columnas de ancho
+    panelBusqueda.add(txtCodigoBuscar);   
+    JButton btnBuscar = new JButton("Buscar");
+    panelBusqueda.add(btnBuscar);
+    add(panelBusqueda, BorderLayout.NORTH);
 }
