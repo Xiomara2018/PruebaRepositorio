5ffcbce07eff05f94c98f7679f6d832cc76c9eb3
Xiomara Nuñez <xiomaranunezccupa@gmail.com>
2026-06-30 00:49:16 -0500

    creamos la clase para el boton 2 DialogoMostrarLibros

---
+++ vistas/DialogoMostrarLibros.java
@@ -0,0 +1,12 @@
+package vistas;
+
+import controladores.GestorBiblioteca;
+import modelos.Libro;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.util.List;
+
+public class DialogoMostrarLibros extends JDialog {
+    
+}
