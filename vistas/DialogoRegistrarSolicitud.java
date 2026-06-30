d288c0dec4adecc75dbcc0f148b0668ac3a37ee9
Xiomara Nuñez <xiomaranunezccupa@gmail.com>
2026-06-30 01:17:15 -0500

    agregamos detalles

--- vistas/DialogoRegistrarSolicitud.java
+++ vistas/DialogoRegistrarSolicitud.java
@@ -4,3 +4,16 @@ import controladores.GestorBiblioteca;
 import estructuras.ExceptionIsEmpty;
 import javax.swing.*;
 import java.awt.*;
+
+public class DialogoRegistrarSolicitud extends JDialog {
+
+    private JTextField txtCodigoEstudiante;
+    private JTextField txtNombreEstudiante;
+    private JTextField txtCodigoLibro;
+    private JTextField txtFecha;
+
+    public DialogoRegistrarSolicitud(JFrame ventanaPadre, GestorBiblioteca gestor) {
+        super(ventanaPadre, "Registrar Solicitud de Préstamo", true);
+        setSize(400, 300);
+        setLocationRelativeTo(ventanaPadre);
+        setLayout(new BorderLayout(10, 10));
