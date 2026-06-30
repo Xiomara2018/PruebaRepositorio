577b5786253f50fad5686e4d582ca60904a09d25
Xiomara Nuñez <xiomaranunezccupa@gmail.com>
2026-06-30 01:01:35 -0500

    estructuramos la clase, como se vera

--- vistas/DialogoBuscarLibro.java
+++ vistas/DialogoBuscarLibro.java
@@ -1,5 +1,18 @@
 package vistas;
 
-public class DialogoBuscarLibro {
+import controladores.GestorBiblioteca;
+import estructuras.ExceptionIsEmpty;
+import estructuras.ItemNotfound;
+import modelos.Libro;
+import javax.swing.*;
+import java.awt.*;
+public class DialogoBuscarLibro extends JDialog{
+    private JTextField txtCodigoBuscar;
+    private JTextArea txtResultado;
+public DialogoBuscarLibro(JFrame ventanaPadre, GestorBiblioteca gestor) {
+        super(ventanaPadre, "Buscar Libro por Código", true);
+        setSize(400, 300);
+        setLocationRelativeTo(ventanaPadre);
+        setLayout(new BorderLayout(10, 10));
     
 }
