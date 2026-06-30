7dad624c74397ddeed92c403654e77d7537d864f
Xiomara Nuñez <xiomaranunezccupa@gmail.com>
2026-06-30 00:14:33 -0500

    creamos botones

--- vistas/VentanaPrincipal.java
+++ vistas/VentanaPrincipal.java
@@ -9,6 +9,13 @@ public class VentanaPrincipal extends JFrame
 public VentanaPrincipal() {
     gestor = new GestorBiblioteca();
     setTitle("QuickLibrary - Sistema de Gestión");
-        setSize(400, 350);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setSize(400, 350);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLocationRelativeTo(null);
+    setLayout(new GridLayout(5, 1, 10, 10));
+
+    JButton btnRegistrarLibro = new JButton("1. Registrar Libro");
+        JButton btnMostrarLibros = new JButton("2. Mostrar Todos los Libros");
+        JButton btnRegistrarSolicitud = new JButton("7. Registrar Solicitud de Préstamo");
+
 }
